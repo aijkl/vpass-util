6e39e892828a3f10a0ff3fee1f33f44949c671c7
aijkl <aijkl@outlook.jp>
2023-04-25 01:20:34 +0900

    引数を入力した時は対話しない

--- VpassUtil/Commands/DownloadCommand.cs
+++ VpassUtil/Commands/DownloadCommand.cs
@@ -10,9 +10,9 @@ namespace VpassUtil.Cli.Commands
     {
         public override async Task<int> ExecuteAsync(CommandContext context, DownloadSettings settings)
         {
-            settings.Cookie = AnsiConsole.Ask<string>("Cookieを入力してください、ブラウザのF12を押してネットワークタブから取得してください");
-            settings.Start = AnsiConsole.Ask<string>("開始を表すyyyy-mmを入力してください");
-            settings.End = AnsiConsole.Ask<string>("終了を表すyyyy-mmを入力してください");
+            if (string.IsNullOrWhiteSpace(settings.Cookie)) settings.Cookie = AnsiConsole.Ask<string>("Cookieを入力してください、ブラウザのF12を押してネットワークタブから取得してください");
+            if (string.IsNullOrWhiteSpace(settings.Start)) settings.Start = AnsiConsole.Ask<string>("開始を表すyyyy-mmを入力してください");
+            if (string.IsNullOrWhiteSpace(settings.End)) settings.End = AnsiConsole.Ask<string>("終了を表すyyyy-mmを入力してください");
 
             var directoryInfo = Directory.CreateDirectory(settings.OutputDirectory);
             using var vpass = new VPassApiClient(settings.Cookie);
