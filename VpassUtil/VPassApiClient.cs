2c1d63b321425b407f1d519cf069ee0e21084c2c
aijkl <aijkl@outlook.jp>
2023-04-25 13:23:51 +0900

    コア機能をVpassUtilに移動した

--- VpassUtil/VPassApiClient.cs
+++ VpassUtil/VPassApiClient.cs
@@ -2,23 +2,23 @@
 
 namespace VpassUtil;
 
-internal class VPassApiClient : IDisposable
+public class VPassApiClient : IDisposable
 {
     private readonly string _cookie;
     public static readonly string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
     private readonly HttpClient _httpClient;
-    internal VPassApiClient(string cookie)
+    public VPassApiClient(string cookie)
     {
         _cookie = cookie;
         _httpClient = new HttpClient();
     }
-    internal async Task<string> FetchDetailCsvAsync(int year, int month)
+    public async Task<string> FetchDetailCsvAsync(int year, int month)
     {
         var request = CreateHistoryRequest(year, month);
         using var response = await _httpClient.SendAsync(request);
         return Encoding.GetEncoding("SJIS").GetString(await response.Content.ReadAsByteArrayAsync());
     }
-    internal HttpRequestMessage CreateHistoryRequest(int year, int month)
+    public HttpRequestMessage CreateHistoryRequest(int year, int month)
     {
         const string baseUrl = "https://www.smbc-card.com/memapi/jaxrs/dl/web_meisai/web_meisai_csv_exec/v1";
         var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, $"{baseUrl}?downloadKey=4&seikyuym={year}{month.ToString().PadLeft(2, '0')}&sk=&downloadtype=csv&mk=1");
