9914a7a3407f6dc470c6e70c2eeb23295528fe0d
aijkl <aijkl@outlook.jp>
2023-04-25 20:55:53 +0900

    fix: コンストラクが有ったらCSVのパースがうまくいかない

--- VpassUtil/Payments.cs
+++ VpassUtil/Payments.cs
@@ -2,11 +2,6 @@ namespace VpassUtil;
 
 public class PaymentDetail
 {
-    public PaymentDetail(DateTime timestamp)
-    {
-        Timestamp = timestamp;
-    }
-
     public DateTime Timestamp { set; get; }
     public string StoreName { set; get; } = "";
     public int Amount { set; get; }
